8de68964f4bd436f7c361ed73f2d8b5fe6f5133c
katsuya <basketball.katsu@ezweb.ne.jp>
2020-08-09 14:45:08 +0900

    change cousol hotelsindex2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,2 @@
 class UsersController < ApplicationController
-  has_many :hotels, dependent: :destroy
-  has_many :likes, dependent: :destroy
-  has_many :liked_hotels, through: :likes, source: :hotel
-  def already_liked?(hotel)
-    self.likes.exists?(hotel_id: post.id)
-  end
 end
